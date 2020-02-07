44098527e3acd6ca713c65abc26a2223e22715b1
Uilian Gonçalves de Sales <uilian.desales@icloud.com>
2020-02-06 22:41:47 -0500

    Initial commit

--- src/com/javatododia/Main.java
+++ src/com/javatododia/Main.java
@@ -1,8 +1,20 @@
 package com.javatododia;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        try {
+            String s = "/tmp/f.txt"
+            BufferedReader bf = new BufferedReader(new FileReader(new File(s)));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
