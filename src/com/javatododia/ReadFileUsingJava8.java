5fa6ab99f9739052c0994d711f1f4d3056674d66
Uilian Gonçalves de Sales <uilian.desales@icloud.com>
2020-02-10 21:42:09 -0500

    Add ReadFileUsingJava8

---
+++ src/com/javatododia/ReadFileUsingJava8.java
@@ -0,0 +1,15 @@
+package com.javatododia;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ReadFileUsingJava8 {
+
+    public static void main(String[] args) throws IOException {
+
+        Files.list(Paths.get("."))
+                .forEach(System.out::println);
+
+    }
+}
